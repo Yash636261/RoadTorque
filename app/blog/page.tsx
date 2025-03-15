99db68ab422ef63c325608f8c7e0dd93d7d96bf6
Yash Suthar <98970491+Yash636261@users.noreply.github.com>
2025-03-15 13:17:27 +0530

    Fix: typescript error

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -70,7 +70,7 @@ export default async function BlogPage() {
 
       {/* Blog Grid */}
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-        {blogPosts.map((post: Blog) => (
+        {blogPosts?.map((post: Blog) => (
           <Card key={post._id}>
             <div className="aspect-video relative">
               <img
